86bac5dab9de8af13ed3931700e77ed6f5a0aa7e
vasanthanila <vasanth15995@gmail.com>
2016-07-16 17:03:30 +0530

    Add files via upload

---
+++ ARRY.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arry;
+
+/**
+ *
+ * @author Student
+ */
+public class ARRY {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int []a={1,2,4,3,5};
+        for(int i=0;i<5;i++)
+        {
+            if(a[i]==i)
+            {
+                System.out.println("the equal number"+i);
+            }
+        }
+        
+    }
+}
