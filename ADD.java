0f7c27d7546e36b9655110aa5d7218aa040c0180
vasanthanila <vasanth15995@gmail.com>
2016-07-18 17:32:20 +0530

    Add files via upload

---
+++ ADD.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package add;
+
+/**
+ *
+ * @author Student
+ */
+public class ADD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int []a={2,7,11,8,15};
+       int sum=0;
+       for(int i=0;i<5-1;i++)
+       {
+           sum=a[i]+a[i+1];
+       
+       if(sum==9)
+       {
+           System.out.println("the target number is"+sum);
+       }
+    }}s
+}
