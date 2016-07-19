aeef6f63bf4793096c984855faa50f9d069f6d7b
vasanthanila <vasanth15995@gmail.com>
2016-07-19 08:38:33 +0530

    Add files via upload

---
+++ Strn.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strn;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author vasantha nila
+ */
+public class Strn {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int i=Integer.parseInt(s);
+        System.out.println(i);
+    }
+    
+}
