d34e10d35f6abb50351e8aee8601afe7c64d352a
vasanthanila <vasanth15995@gmail.com>
2016-07-21 16:34:06 +0530

    Add files via upload

---
+++ Even22.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package even22;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Student
+ */
+public class Even22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        for(int c=a;c<b;c++)
+        {
+            if(c%2==0)
+            {
+                System.out.println(""+c);
+            }
+        }
+    }
+}
