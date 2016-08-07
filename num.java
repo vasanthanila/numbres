136439dd48e3047cf9f23455b1bf86e098b6d489
vasanthanila <vasanth15995@gmail.com>
2016-08-07 20:32:34 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,28 @@
+package num;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author student
+ */
+public class num {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+int j;
+      String s2="hello";
+for( j=0;j<=s2.length();j++)
+{
+    char e=s2.charAt(i);
+    if((e== 'A')||(e=='E')||(e=='I')||(e=='O')||(e=='U')){
+        {
+String h=s2.substring(0, j);
+
+String g=s2.substring(i+ j);
+s2=h+"Z"+g;
+}}
+ System.out.println(s2);
+}}       
