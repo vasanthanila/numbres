b74762d48e5bc6515f616c3bdf1f10511ae6f60f
vasanthanila <vasanth15995@gmail.com>
2016-08-07 20:48:38 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,8 @@
+package num1;
+public class num1{
+public static void main(String[] args){
+int year = 2000
+if (year % 4 && year%100&&year % 400) 
+   { 
+          System.out.println ("{0} is a leap year"+year);
+     }}}  
